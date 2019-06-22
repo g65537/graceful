8794294a1a1e6298c93e07d1b4ee5a83e1614f86
g65537 <g65537@gmail.com>
2019-06-22 21:38:48 +0800

    just format

--- address.go
+++ address.go
@@ -2,6 +2,6 @@ package graceful
 
 // address defines addr as well as its network type
 type address struct {
-	network string // tcp, unix
 	addr    string // ip:port, unix path
+	network string // tcp, unix
 }
